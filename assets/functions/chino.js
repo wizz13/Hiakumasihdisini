90638e2d3f1c534f029c8f047e86a3c67f55c855
wizz13 <136373874+wizz13@users.noreply.github.com>
2024-05-10 06:40:44 +0700

    Delete assets/functions/chino.js

--- assets/functions/chino.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet() {
-    const max = 267;
-    return new Response(await fetch(`https://raw.githubusercontent.com/AutumnVN/chino.webp/main/${Math.ceil(Math.random() * max)}.webp`).then(r => r.blob()), {
-        headers: {
-            'content-type': 'image/webp',
-            'cache-control': 'no-cache, no-store, must-revalidate, max-age=0'
-        }
-    });
-}
